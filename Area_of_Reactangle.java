22101c5a09ec71914378af25b159ff1d9be2a4c4
Kuldeep2207 <128211235+Kuldeep2207@users.noreply.github.com>
2023-03-26 23:48:03 +0530

    Upload program area of rectangle
    Upload program area of rectangle using Instance variable

---
+++ Area_of_Reactangle.java
@@ -0,0 +1,10 @@
+class Area_of_Reactangle{
+    float len = 4.0f ,brea = 2.0f;
+    double ARI;
+    public static void main(String rect[])
+    {
+        Area_of_Reactangle obj = new Area_of_Reactangle();
+        obj.ARI = obj.len *obj.brea;
+        System.out.println("Area of Reactangle ="+obj.ARI);
+  }
+}
