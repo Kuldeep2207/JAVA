0cb5ecc5b1f8fcc1bdbedf76129e0013f7326f15
Kuldeep2207 <128211235+Kuldeep2207@users.noreply.github.com>
2023-03-26 23:44:36 +0530

    Upload program of area of circle using variable

---
+++ Area_of_circle2.java
@@ -0,0 +1,10 @@
+class Area_of_circle2{
+    public static void main(String arg[])
+    {
+        double radius=4.5;
+        double area;
+        area = 3.14*radius*radius;
+        System.out.println("Area of circle using local variable");
+        System.out.println("Area of circle="+area);
+    }
+}
