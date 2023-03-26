e304ca42cecd9e06d408c3c9164c69c36332b640
Kuldeep2207 <128211235+Kuldeep2207@users.noreply.github.com>
2023-03-26 23:57:33 +0530

    Upload average of number program
    Upload average of number program using local variable

---
+++ Avarage2.java
@@ -0,0 +1,13 @@
+public class Avarage2 {
+    public static void main(String arg[])
+    {
+        int num1=97 , num2=95 ,num3=96;
+        double avg , sum =0;
+        sum = num1 +num2 +num3 ;
+        avg = sum/3;
+        System.out.println("Average of numbers using local variable");
+        System.out.println("Sum of numbers ="+sum);
+        System.out.println("Average ="+avg);
+    }
+    
+}
