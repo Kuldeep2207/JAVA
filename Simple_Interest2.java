415627512e26fe19ee130816a6507d8153f7a1ab
Kuldeep2207 <128211235+Kuldeep2207@users.noreply.github.com>
2023-03-26 23:54:05 +0530

    Upload simple interest program
    Upload program simple interest using local variable

---
+++ Simple_Interest2.java
@@ -0,0 +1,11 @@
+public class Simple_Interest2 {
+    public static void main(String arg[])
+    {
+        double principle=2356.50 ,rate=5, time=2;
+        double SI;
+        SI = (principle*rate*time)/100;
+        System.out.println("Simple Interest using local varible");
+        System.out.println("Simple Interest ="+SI);
+    }
+    
+}
