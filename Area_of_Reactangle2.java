d55aeabfc795abab994b8cfd5dc85b82c6dbc91c
Kuldeep2207 <128211235+Kuldeep2207@users.noreply.github.com>
2023-03-26 23:49:47 +0530

    Upload program area of rectangle
    Upload program area of rectangle using local variable

---
+++ Area_of_Reactangle2.java
@@ -0,0 +1,10 @@
+class Area_of_Reactangle2{
+    public static void main(String arg[])
+    {
+        double len=4.5 , brea=5.5;
+        double area;
+        area = len * brea;
+        System.out.println("Area of Reactangle using local varible");
+        System.out.println("Area of Reactangle="+area);
+    }
+}
